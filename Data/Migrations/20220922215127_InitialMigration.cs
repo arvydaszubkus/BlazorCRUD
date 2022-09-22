1cc5bbf142991edea58cc96c342420fa06b3c83a
Arvydas <zubkus.arvydas@gmail.com>
2022-09-22 23:38:16 +0100

    Add project files.

---
+++ Data/Migrations/20220922215127_InitialMigration.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorCRUD.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", maxLength: 128, nullable: false),
+                    Content = table.Column<string>(type: "TEXT", maxLength: 4096, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 1, "This content for advertisement 1.", "Advertisement 1" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 2, "This content for advertisement 2.", "Advertisement 2" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 3, "This content for advertisement 3.", "Advertisement 3" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 4, "This content for advertisement 4.", "Advertisement 4" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 5, "This content for advertisement 5.", "Advertisement 5" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 6, "This content for advertisement 6.", "Advertisement 6" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 7, "This content for advertisement 7.", "Advertisement 7" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 8, "This content for advertisement 8.", "Advertisement 8" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 9, "This content for advertisement 9.", "Advertisement 9" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 10, "This content for advertisement 10.", "Advertisement 10" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
